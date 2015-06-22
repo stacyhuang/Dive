a8bff9ef73b7ccf876728d5d7d7178b4e236600c
Jack McDevitt <jackmcdevitt4@gmail.com>
2015-06-22 16:19:17 -0700

    Direction working, image not following direction

--- public/www/js/main/mainCtrl.js
+++ public/www/js/main/mainCtrl.js
@@ -5,19 +5,37 @@
 
     .controller('MainCtrl', MainCtrl);
 
-  MainCtrl.$inject = ['$scope', 'CardService'];
+  MainCtrl.$inject = ['$scope', 'CardService', '$ionicGesture'];
 
-  function MainCtrl($scope, CardService){
-        $scope.card = CardService.card;
-        console.log($scope.card)
-        $scope.cardSwipedLeft = function(){
-            CardService.plusLeft();
-            CardService.addCard();
-        }
-        $scope.cardSwipedRight = function(){
-            CardService.plusRight();
-            CardService.addCard();
-        }
+  function MainCtrl($scope, CardService, $ionicGesture, TDCardDelegate){
+        $scope.card = CardService.card();
+    var element = angular.element(document.querySelector('#eventPlaceholder'));
+            var events = [{
+            event: 'dragup',
+            },{
+            event: 'dragdown',
+            },{
+            event: 'dragleft',
+            },{
+            event: 'dragright',
+            }];
+    angular.forEach(events, function(obj){
+    var dragGesture = $ionicGesture.on(obj.event, function (event) {
+        $scope.$apply(function () {
+            $scope.lastEventCalled = obj.text;
+            //console.log(obj.event)
+            if (obj.event == 'dragleft'){                           
+                CardService.plusLeft();
+                $scope.card = CardService.addCard();
+            }
+            if (obj.event == 'dragright'){                          
+                CardService.plusRight();
+                $scope.card = CardService.addCard();
+            }
+        });
+
+        }, element);
+    }); 
   }
 
 })();
