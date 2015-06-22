47cf8d9f2318ce4e5839001eb870d23508624d0c
Jack McDevitt <jackmcdevitt4@gmail.com>
2015-06-22 13:44:38 -0700

    Shows swipe card, and maybe gesture functionality?

--- public/www/js/main/mainCtrl.js
+++ public/www/js/main/mainCtrl.js
@@ -5,10 +5,11 @@
 
     .controller('MainCtrl', MainCtrl);
 
-  MainCtrl.$inject = ['$scope'];
+  MainCtrl.$inject = ['$scope', 'CardService'];
 
-  function MainCtrl($scope){
-    .controller('MainCtrl', function($scope, CardService){
+  function MainCtrl($scope, CardService){
+        $scope.card = CardService.card;
+        console.log($scope.card)
         $scope.cardSwipedLeft = function(){
             CardService.plusLeft();
             CardService.addCard();
@@ -17,7 +18,6 @@
             CardService.plusRight();
             CardService.addCard();
         }
-    })
   }
 
 })();
