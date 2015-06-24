7d610a9c7b44847708e5ab5fdaa02e2491d23859
Jack McDevitt <jackmcdevitt4@gmail.com>
2015-06-23 17:05:22 -0700

    Continuality for places to show up through algo-call created

--- public/www/js/main/mainService.js
+++ public/www/js/main/mainService.js
@@ -8,19 +8,26 @@
 
      // CardService.$inject = ['LocationFactory'];
 
-     function CardService(){
+     function CardService($rootScope){
         this.i = 0;
         this.info;
         this.retrieve = function(data){
             this.info = data;
             console.log(this.info[0])
+            this.i = 0;
         }
         this.card = function(){
-            return {image: "./img/background.jpg", name: "Homepage", info: "this is where it all begins, can you see the shingingwdoia aopufia aodsf s d"}
+            return {image_url: "./img/background.jpg", name: "Homepage", location: {address: ["this is where it all begins, can you see the shingingwdoia aopufia aodsf s d"]}}
         }
         this.addCard = function(){
+            if(this.i === 20){
+                console.log($rootScope)
+                $rootScope.$broadcast('newInfo');
+               }
+            else{
+                return this.info[this.i]
+            }
             //Add a card to the ion-pane
-            return this.info[this.i]
         }
         this.plusLeft = function(){
             console.log('left')
