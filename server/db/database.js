b6ab7bd9a330b71e86d521cbc63c1dcc860c9a87
John Paulino <paulinoj@gmail.com>
2015-06-26 10:39:38 -0700

    Changed redis server

--- server/db/database.js
+++ server/db/database.js
@@ -3,7 +3,19 @@ var redis = require('redis');
 // By default, redis.createClient() will use 127.0.0.1 and 6379 as the hostname and port respectively. 
 // var client = redis.createClient(port, host);
 
-var client = redis.createClient();
+console.log("process.env.REDISTOGO_URL: ", process.env);
+if (process.env.REDISTOGO_URL) {
+    // TODO: redistogo connection
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    var client = redis.createClient(rtg.port, rtg.hostname);
+    console.log('rtg:', rtg);
+
+    client.auth(rtg.auth.split(":")[1]);
+} else {
+    var client = redis.createClient();
+}
+
+
 module.exports.client = client;
 client.on('connect', function() {
     console.log('connected');
