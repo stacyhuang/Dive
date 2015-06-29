690c52cb3dee1ba95d7fe1cdbb22000c870762e1
Bryan Liu <bsliu17@gmail.com>
2015-06-29 10:34:44 -0700

    remove old redis config

--- server/db/config.js
+++ server/db/config.js
@@ -1,15 +1,15 @@
-var redis = require('redis');
-
-
-// By default, redis.createClient() will use 127.0.0.1 and 6379 as the hostname and port respectively. 
-//var client = redis.createClient(port, host);
-var client = redis.createClient();
-client.on('connect', function() {
-    console.log('Commander, you are connected');
-});
-
-client.flushdb();
-module.exports.client;
+// var redis = require('redis');
+//
+//
+// // By default, redis.createClient() will use 127.0.0.1 and 6379 as the hostname and port respectively.
+// //var client = redis.createClient(port, host);
+// var client = redis.createClient();
+// client.on('connect', function() {
+//     console.log('Commander, you are connected');
+// });
+//
+// client.flushdb();
+// module.exports.client;
 
 
 // var mongoose = require('mongoose');
